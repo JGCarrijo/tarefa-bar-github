03d5d8d92a443fd681bddd2a78d0650ec41c30be
Joao Guilherme <carrijojg@gmail.com>
2020-05-07 19:48:40 -0300

    Leitura dos dados

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main()
 {
+    string genero;
+
+    int cerveja, refrigerante, espeto;
+
+    cout << "Sexo: ";
+    cin >> genero;
+
+    cout << "Quantidade de cervejas: ";
+    cin >> cerveja;
+
+    cout << "Quantidade de refrigerantes: ";
+    cin >> refrigerante;
+
+    cout << "Quantidade de espetinhos: ";
+    cin >> espeto;
+
 
     return 0;
 }
