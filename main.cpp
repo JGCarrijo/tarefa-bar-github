12c3942c36b5c890cababd7b93e9ea9ee7543dad
Joao Guilherme <carrijojg@gmail.com>
2020-05-07 20:07:29 -0300

    Relatorio gerado

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@ using namespace std;
 int main()
 {
     string genero;
-
     int cerveja, refrigerante, espeto;
+    double consumo, couvert, ingresso, total;
 
     cout << "Sexo: ";
     cin >> genero;
@@ -19,6 +19,39 @@ int main()
 
     cout << "Quantidade de espetinhos: ";
     cin >> espeto;
+
+
+    if (genero == "F") {
+        ingresso = 8.0;
+    }
+    else {
+        ingresso = 10.0;
+    }
+
+    consumo = cerveja * 5.0 + refrigerante * 3.0 + espeto * 7.0;
+
+    if(consumo > 30.0) {
+        couvert = 0.0;
+    }
+    else {
+        couvert = 4.0;
+    }
+
+    total = ingresso + consumo + couvert;
+
+    cout << fixed << setprecision (2);
+    cout << endl << "RELATORIO:" << endl;
+    cout << "Consumo = R$ " << consumo << endl;
+
+    if (couvert == 0.0) {
+        cout << "Isento de Couvert" << endl;
+    }
+    else{
+        cout << "Couvert = R$" << couvert << endl;
+    }
+
+    cout << "Ingresso = R$ " << ingresso << endl;
+    cout << endl << "Valor a pagar = R$" << total << endl;
 
 
     return 0;
